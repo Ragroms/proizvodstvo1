e6ba1330e114d0296bd98b9bff2d5ed58dc353f0
Ragroms <127696223+Ragroms@users.noreply.github.com>
2023-06-19 02:27:23 +0300

    Add files via upload

---
+++ Задание 3/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Задание_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit;
+            double interestRate;
+
+            try
+            {
+                Console.Write("Введите сумму вклада: ");
+                deposit = Convert.ToDouble(Console.ReadLine());
+
+                if (deposit < 100)
+                {
+                    interestRate = 0.05;
+                }
+                else if (deposit >= 100 && deposit <= 200)
+                {
+                    interestRate = 0.07;
+                }
+                else
+                {
+                    interestRate = 0.1;
+                }
+
+                double interest = deposit * interestRate;
+                double total = deposit + interest;
+
+                Console.WriteLine("Начисленные проценты: " + interest);
+                Console.WriteLine("Общая сумма: " + total);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка!!! Введите число!");
+            }
+        }
+    }
+}
