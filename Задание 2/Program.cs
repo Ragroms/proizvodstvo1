c756aefa46ff556aa700be730ce952c3e1aeb02f
Ragroms <127696223+Ragroms@users.noreply.github.com>
2023-06-19 02:26:52 +0300

    Add files via upload

---
+++ Задание 2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Задание_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число от 0 до 10: ");
+
+            try
+            {
+                int X = int.Parse(Console.ReadLine());
+
+                if (X > 5 && X < 10)
+                {
+                    Console.WriteLine($"{X} попадает в диапазон 5-10");
+                }
+                else
+                {
+                    Console.WriteLine($"{X} не попадает в диапазон 5-10");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка!!! Введино не число!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
