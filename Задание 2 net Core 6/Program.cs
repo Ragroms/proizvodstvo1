1db72bd73c837ef8bd9524d27ad8e5756b26f731
Ragroms <127696223+Ragroms@users.noreply.github.com>
2023-06-19 02:29:31 +0300

    Add files via upload

---
+++ Задание 2 net Core 6/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Задание_2_net_Core_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+              Console.WriteLine("Введите текст:");
+              string text = Console.ReadLine();
+              string path = "file.txt";
+              File.WriteAllText(path, text);
+              string[] words = text.Split(new char[] { ' ', '.', ',', '!', '?', ';', ':', '1', '2', '3', '4', '5', '6', '7','8', '9', '0', '-', '=', '+', '*','/', '(', ')', '<','>','"','`', '~', '|', '#', '№', '@', '%', '^', '&','_'}, StringSplitOptions.RemoveEmptyEntries);
+              Console.WriteLine("Количество слов в тексте: " + words.Length);
+            }
+        }
+    }
+}
